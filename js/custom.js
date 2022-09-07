16f3d3051793d757ef46f957732f66d020ef1c82
SergioEMP <sergio.martinez@nivelics.co>
2022-09-07 16:11:37 -0500

    Add Bussiness section mobile styles

--- js/custom.js
+++ js/custom.js
@@ -120,7 +120,8 @@ $(document).ready(function(){
 
 $(document).ready(function(){
   $('.bussiness__slider').slick({
-    infinite: false,
+    dots: true,
+    infinite: true,
     speed: 300,
     slidesToShow: 4,
     slidesToScroll: 1,
